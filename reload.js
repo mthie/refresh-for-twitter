ddbb1a839c23ff7bbf5b030ec77cf4d37671c21f
Martin Thielecke <github@mthie.com>
2019-07-16 13:29:50 +0200

    skip hidden "new tweets" button

--- reload.js
+++ reload.js
@@ -2,6 +2,10 @@ function checkNewTweetsRefresh() {
   if(document.readyState != 'interactive') {
     let tags = document.querySelectorAll('*[aria-label^="new tweets" i]');
     for (let tag of tags) {
+      if (tag.hasAttribute("aria-hidden") && tag.getAttribute("aria-hidden") === "true") {
+        continue;
+      }
+
       var evt = document.createEvent('Events');
       evt.initEvent('click', true, false);
       tag.dispatchEvent(evt);
@@ -10,5 +14,6 @@ function checkNewTweetsRefresh() {
 }
 
 (function() {
+  console.log("Refresh for Twitter started");
   window.addEventListener('scroll', checkNewTweetsRefresh);
 })();
