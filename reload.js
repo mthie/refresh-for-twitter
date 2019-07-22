922437e115e91a49a1f2db1cf51bcb7ece7a208b
Martin Thielecke <github@mthie.com>
2019-07-22 22:20:53 +0200

    hopefully fix the #newnewtwitter thing

--- reload.js
+++ reload.js
@@ -1,22 +1,39 @@
+var previousTweets = 0;
+var firstTweet = null;
+var inView = true;
+
 function checkNewTweetsRefresh() {
   if(document.readyState != 'interactive') {
-    let tags = document.querySelectorAll('*[aria-label^="new tweets" i]');
-    for (let tag of tags) {
-      if (tag.hasAttribute("aria-hidden") && tag.getAttribute("aria-hidden") === "true") {
-        continue;
-      }
+    let tags = document.querySelectorAll("div[aria-label^='timeline' i]>div:first-child>div:first-child");
+    if (tags.length < 1) {
+      return;
+    }
+    let firstTag = tags[0].firstChild;
 
-      console.log("New tweets found", tag);
-      var evt = document.createEvent('Events');
-      evt.initEvent('click', true, false);
-      tag.firstChild.dispatchEvent(evt);
-      window.setTimeout(function() {
-        window.scrollTo({
-          top: 0,
-          left: 0,
-          behavior: 'smooth'
-        });
-      }, 600);
+    inView = true
+    let stillInList = false
+    for (let node of tags[0].children) {
+      if (node == firstTweet) { stillInList = true }
+    }
+    if (!stillInList) {
+      firstTweet = tags[0].firstChild;
+    }
+
+    if (window.pageYOffset == 0) {
+      firstTweet = tags[0].firstChild;
+    }
+
+    if (firstTweet !== null) {
+      let tweetBounding = firstTweet.getBoundingClientRect();
+      if (tweetBounding.top < 0 ||
+          tweetBounding.bottom > window.innerHeight) {
+        inView = false
+      }
+    }
+
+    if (firstTweet != firstTag && inView) {
+      firstTweet = firstTag
+      window.scrollTo(0, 0)
     }
   }
 }
